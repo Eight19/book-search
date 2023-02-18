61b272d0003418a1c34a13443b7d6d178fbc99ad
Tiffany Jones <Eight19Realty@gmail.com>
2023-02-18 10:30:32 -0500

    Update auth.js

--- utils/auth.js
+++ utils/auth.js
@@ -1,3 +1,5 @@
+//Received assitance from tutors and instructional staff//
+
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
@@ -15,25 +17,28 @@ module.exports = {
       token = token.split(' ').pop().trim();
     }
 
+    // if no token, return request object as is
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
-    // verify token and get user data out of it
+    // decode and attach user data to request object
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-    } catch {
+    }
+    catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+    // return updated request object
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+  }
 };
+
+   
